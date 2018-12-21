c2b78691e17591d79f256405f430a62c52f623d3
benlr98 <benlr98@gmail.com>
2018-12-20 20:01:52 -0500

    Add all Components

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-recipe-list',
+    templateUrl: './recepe-list.component.html',
+    styleUrls: ['./recipe-list.component.css']
+})
+
+export class RecipeListComponent {
+
+}
