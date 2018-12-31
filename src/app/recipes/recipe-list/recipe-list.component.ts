27b5a0303323685da67abcee1c44ecffebacffbc
benlr98 <benlr98@gmail.com>
2018-12-31 10:15:31 -0500

    created ingredients model

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -10,7 +10,8 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
 
     recipes: Recipe[] = [
-        new Recipe('Ramen Noodles', 'A quick snack', 'https://cdn.pixabay.com/photo/2016/02/22/17/05/food-1216048_1280.jpg')
+        new Recipe('Ramen Noodles', 'A quick snack', 'https://cdn.pixabay.com/photo/2016/02/22/17/05/food-1216048_1280.jpg'),
+        new Recipe('Mac & Cheese', 'Made for cheese lovers', 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Original_Mac_n_Cheese_.jpg/900px-Original_Mac_n_Cheese_.jpg')
     ];
 
     constructor() {}
